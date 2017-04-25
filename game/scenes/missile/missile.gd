4015bbb03965d8146719c7cdcb32299ab1c60a39
CrazyGuy108 <taylor@hansenfamily.us>
2017-04-24 17:04:48 -0700

    Add lifetime to missiles
    The missile now teleports to the other side of the viewport if it goes too far, but will also die out after its lifetime goes down to 0.

--- game/scenes/missile/missile.gd
+++ game/scenes/missile/missile.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal destroyed
 const HIT_ASTEROID = true
 export(float) var speed = 256.0 # pixels per second
+export(float) var lifetime = 1.0 # seconds
 
 func _ready():
 	set_fixed_process(true)
@@ -10,12 +11,22 @@ func _ready():
 func _fixed_process(delta):
 	_move(delta)
 	_check_asteroids()
-	if (_is_out_of_bounds()):
+	lifetime -= delta
+	if (lifetime < 0.0):
 		emit_signal("destroyed", self)
 
 # moves according to the speed
 func _move(delta):
 	move_local_y(speed * delta, true)
+	# account for the missile going out of bounds (teleport to the other side)
+	var pos = get_pos()
+	var size = get_viewport_rect().size
+	pos = Vector2(fmod(pos.x, size.x), fmod(pos.y, size.y))
+	if (pos.x < 0):
+		pos.x += size.x
+	if (pos.y < 0):
+		pos.y += size.y
+	set_pos(pos)
 
 # checks if it hit any asteroids
 func _check_asteroids():
